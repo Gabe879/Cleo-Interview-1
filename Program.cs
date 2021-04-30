e990d5ec8dca49371b1593b6a4c5730398f40130
Gabe879 <gabewohlwend@gmail.com>
2021-04-30 13:54:21 -0500

    Initial program checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cleo_Interview_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
